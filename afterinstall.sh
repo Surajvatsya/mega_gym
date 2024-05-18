5b1e7a9caa8d1dbfdec76780fad5a75f55ab910b
suraj.kumar1 <suraj.kumar1@juspay.in>
2024-05-18 16:38:27 +0530

    feat: modified afterinstall

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,2 +1,13 @@
 cd /home/ubuntu
+
+#!/bin/bash
+sudo apt-get -y update
+
+# INSTALL NODEJS - ONLY FOR UBUNTU
+sudo apt install nodejs -y
+
+# INSTALL NPM & PM2 - ONLY FOR UBUNTU
+sudo apt install npm -y
+
+
 npm -f install
