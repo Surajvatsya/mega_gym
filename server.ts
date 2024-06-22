08499510e37d457584b36e54758924e49a2375c3
suraj.kumar1 <suraj.kumar1@juspay.in>
2024-06-22 23:06:54 +0530

    feat: fixed updateSubscription2

--- server.ts
+++ server.ts
@@ -72,43 +72,7 @@ const reminderJob = cron.schedule("0 8 * * * *", async () => {
 });
 
 //solve issue of 1st date here
-const markAbsent = cron.schedule("0 0 * * * *", async () => {
-    try{
-        const today = new Date().getDate();
-        if(today == 1){
-            Customer.find().exec().then(async (customer:any) => {
-                if (customer){
-                    const createAttandanceRecord = new Attendance({
-                        _id: new mongoose.Types.ObjectId(),
-                        customerId: customer._id,
-                        year: new Date().getFullYear(),
-                        month: new Date().getMonth() + 1,
-                        days: ""
-                      })
-                  await createAttandanceRecord.save();
-                }
-                
-            })
-        }
-        Attendance.find().exec().then((customerAttendance:any) => {
-            if (customerAttendance) { // Check if customerAttendance exists
-                if (customerAttendance.days.length != today) {
-                    customerAttendance.days += '0';
-                    customerAttendance.save();
-                }
-            } else {
-                console.log("No attendance record found!"); // Handle no record scenario
-            }
-        });
-    }
-    catch (error) {
-        console.log("Error", error);
-      }
-    
-});
-
 reminderJob.start();
-markAbsent.start();
 
 const port = process.env.PORT;
 
